300a67078efdb55d94ef7f7ee9420f0a9f6e70b3
Adamczak <krystian@debian>
2013-10-25 18:21:03 +0200

    wyliczanie epsilona za pomoca kahana 3(4/3-1)-1

---
+++ numerkiL1/z2.c
@@ -0,0 +1,31 @@
+/* 
+ * File:   z2.c
+ * Author: krystian
+ *
+ * Created on 25 październik 2013, 17:08
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    float epsFloat;
+    double epsDouble;
+    long double epsLDouble;
+
+    epsFloat = 3.0 * (4.0 / 3.0 - 1.0) - 1.0;
+    epsDouble = 3.0 * (4.0 / 3.0 - 1.0) - 1.0;
+    epsLDouble = 3.0 * (4.0 / 3.0 - 1.0) - 1.0;
+    printf("===EPSILONY===\n");
+    printf("float = %e\ndouble = %e\nlong double = ", epsFloat, epsDouble);
+    printf("%Le", epsLDouble);
+    printf("\n===float.h===\n");
+    printf("float = %e\ndouble = %e\nlong double = %Le", FLT_EPSILON, DBL_EPSILON, LDBL_EPSILON);
+    
+    return (EXIT_SUCCESS);
+}
+
